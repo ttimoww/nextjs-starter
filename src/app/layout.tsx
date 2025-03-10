0159ed724226bb8bcef29686354dca6c3c0fc7c4
Timo <ttimoww@gmail.com>
2025-03-10 12:13:02 +0100

    yolo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from 'sonner';
 import './globals.css';
 
-import { TanstackQueryProvider } from '@/components/tanstack-query-provider';
+import { TanstackQueryProvider } from '@/components/providers/tanstack-query';
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
 	return (
